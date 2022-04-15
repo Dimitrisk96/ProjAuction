25fdb872a1d42c885440ea479cef212352261f69
Dimitrisk96 <80197498+Dimitrisk96@users.noreply.github.com>
2022-04-15 14:21:25 +0300

    Add files via upload

---
+++ code/Admin.java
@@ -0,0 +1,31 @@
+public class Admin {
+    private String username;
+    
+    private String password;
+    
+    private String email;
+    
+    private String name;
+    
+    private String surname;
+    
+    private String ko;
+    
+    
+    public Admin(String username, String password, String email, String name, String surname, String ko)
+            
+    {
+        this.username = username;
+        
+        this.password = password;
+        
+        this.email = email;
+        
+        this.name = name;
+        
+        this.surname = surname;
+        
+        this.ko = ko;
+    }
+    
+}
