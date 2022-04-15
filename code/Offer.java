1f41baad8b0b32e5e317055e19ea6d7c1b713254
trivellas <103255450+trivellas@users.noreply.github.com>
2022-04-15 14:58:28 +0300

    Add files via upload

---
+++ code/Offer.java
@@ -0,0 +1,33 @@
+package com.mycompany.projauction;
+import java.sql.Date;
+
+
+
+
+public class Offer {
+    
+    private String username_freelancer;
+        
+    private int project_request_id;
+    
+    private int bid;
+    
+    private Date date;
+    
+    private String status;
+    
+    
+    
+    public Offer(String username_freelancer, int project_request_id, int bid, Date date, String status)
+    
+    {
+       this.username_freelancer = username_freelancer;
+        
+       this.project_request_id = project_request_id;
+    
+       this.bid = bid;
+    
+       this.date = date;
+    
+       this.status = status;
+}}
