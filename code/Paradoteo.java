2853d10b3a3131c686855e3fb20621d6f4983027
Dimitrisk96 <80197498+Dimitrisk96@users.noreply.github.com>
2022-05-11 15:51:15 +0300

    Add files via upload

--- code/Paradoteo.java
+++ code/Paradoteo.java
@@ -1,12 +1,24 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 
-/**
- *
- * @author dimit
- */
+
+import java.sql.Time;
+import java.util.Date;
+
 public class Paradoteo {
-    
+
+	private String project_id;
+	private String code_paradoteou;
+	private Date date;
+	private Time hour;
+	private String file;
+
+	
+    public Paradoteo(String project_id, String code_paradoteou, Date  date, Time hour, String file)
+	{ 	
+		this.project_id= project_id;
+		this.code_paradoteou = code_paradoteou;
+		this.date = date;
+		this.hour = hour;
+		this.file = file;
+		
+	}
 }
