d098e0765b2d627f2c8291e05a76cf4bda1ab5f6
Dimitrisk96 <80197498+Dimitrisk96@users.noreply.github.com>
2022-05-20 13:29:43 +0300

    Add files via upload

--- code/Offer.java
+++ code/Offer.java
@@ -26,4 +26,63 @@ public class Offer {
        this.date = date;
     
        this.status = status;
-}}
+}
+    String get_String username_freelancer()
+            
+    {
+        return username_freelancer;
+    }
+    
+    int get_project_request_id()
+            
+    {
+        return project_request_id;
+        
+    }
+    
+    int get_bid()
+            
+    {
+        return bid;
+    }
+    
+    Date get_date()
+            
+    {
+        return date;
+    }
+    
+    String get_status()
+            
+    {
+        
+        return status;
+    }
+
+     void set_username_freelancer(String username_freelancer)
+            
+    {
+        this.username_freelancer = username_freelancer;
+    }
+    void set_project_request_id(int project_request_id)
+            
+    {
+        this.project_request_id = project_request_id;
+    }
+    void set_bid(int bid)
+            
+    {
+        this.bid= bid;
+    }
+    void set_date(Date date)
+            
+    {
+        this.date = date;
+    }
+    void set_status(String status)
+            
+    {
+        this.status= status;
+    }
+
+}
